873fcfa4f5f51c44aa025703712990385af80051
AbaadirYasin <145667340+AbaadirYasin@users.noreply.github.com>
2023-11-22 11:28:19 +0000

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,25 @@
 import React from "react";
-import BlogPost from "./BlogPost";
+import NavBar from "./NavBar";
+import Home from "./Home";
+import About from "./About";
+import Links from "./Links";
+
+// pass this data down as props to the child component(s) that need it!
+import user from "../data/user";
+console.log(user);
 
 function App() {
-  return <BlogPost />;
+  return (
+    <div>
+      <NavBar />
+      <Home />
+      <About />
+      <Links />
+      
+
+      
+    </div>
+  );
 }
 
 export default App;
